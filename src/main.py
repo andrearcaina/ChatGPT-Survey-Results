0d9e8192b487c1752f21c27d5efcb305f289e1f2
Andre <dtandre331@gmail.com>
2023-03-09 14:28:17 -0500

    CSV I/O
    - read from a CSV file
    - created separate lists for each row of responses
    - this is so we can graph each list of data using numpy and matplotlib later on
    - wrote to a new CSV file
    - this is for readability and clarity and then can be used on spreadsheets or excel

--- src/main.py
+++ src/main.py
@@ -1,9 +1,39 @@
 import matplotlib.pyplot as plt     # library for graphing data
 import numpy as np                  # library for entering data
+import csv                          # library for reading data 
 
-responses = np.genfromtxt("./data/ChatGPT Generalized Responses.csv",delimiter=',')
+with open("./data/ChatGPT Generalized Responses.csv","r") as f:
+    r = csv.reader(f,delimiter=",")
+    RESPONSES = []
+    for row in r:
+        RESPONSES.append(row)
 
-print(responses)                    # doesn't work. our data is flawed
+AGE           = [row[1] for row in RESPONSES]
+DEPARTMENT    = [row[5] for row in RESPONSES]
+HEARD         = [row[6] for row in RESPONSES]
+USAGE         = [row[7] for row in RESPONSES]
+IMPROVEMENT   = [row[8] for row in RESPONSES]
+RELIABILITY   = [row[9] for row in RESPONSES]
+LEARNING      = [row[10] for row in RESPONSES]
+DISHONESTY    = [row[11] for row in RESPONSES]
+ASSISTANCE    = [row[12] for row in RESPONSES]             
+
+IMPROVED_DATA = [
+                    AGE,
+                    DEPARTMENT,
+                    HEARD,
+                    USAGE,
+                    IMPROVEMENT,
+                    RELIABILITY,
+                    LEARNING,
+                    DISHONESTY,
+                    ASSISTANCE
+                ]
+
+with open("./data/Individual Data.csv","w",newline='') as f:
+    write = csv.writer(f,delimiter=",")
+    for data_row in IMPROVED_DATA:
+        write.writerow(data_row)
 
 '''
     what to do:
