3ab4803f4da7b9103f8edc4510f15234b5070c13
Andre <dtandre331@gmail.com>
2023-03-12 22:34:09 -0400

    Implemented pie graph function
    - plotted pie graph based on data input from button
    - yet to: put percentage, legend, make the UI look better, and do the horizontal bar graphs

--- src/graph.py
+++ src/graph.py
@@ -11,14 +11,32 @@ import numpy as np                                              # library for en
         - use data from read_csv file and output the respective graphs
 '''
 
-# just prints it for now
-def create_pie(data,title):
-    print(title)
-    values, totals = np.unique(data, return_counts=True)
-    print(values, totals)
+def percentage(pct, allvals):
+    absolute = int(np.round(pct / 100. * np.sum(allvals)))
+    return "{:.1f}%\n({:d})".format(pct, absolute)
 
 # just prints it for now
-def create_bar(data,title): 
+def create_pie(frame,data,title):
+    values, totals = np.unique(data, return_counts=True)
+    
+    font = {'family' : 'Georgia',
+            'weight' : 'normal',
+            'size'   : 11}
+
+    plt.rc('font', **font)
+
+    fig = plt.Figure()
+    ax = fig.add_subplot(111)
+    fig.set_facecolor('#D3D3D3')
+    ax.set_title(title, loc='center', wrap=True)
+    ax.pie(totals, radius=0.7, labels=values)
+    
+    #putting the chart onto tkinter
+    chart = FigureCanvasTkAgg(fig,frame)
+    chart.get_tk_widget().place(x=75,y=200)
+
+# just prints it for now
+def create_bar(frame,data,title):
     print(title)
     values, totals = np.unique(data, return_counts=True)
     print(values, totals)
--- src/main.py
+++ src/main.py
@@ -2,81 +2,82 @@ from tkinter import *                          # library for GUI elements
 from graph import (create_pie, create_bar)     # import functions from graph.py
 from readcsv import *                          # use variables from read_csv file
 
-class Window(Tk):
-    def __init__(self):
-        super().__init__()
-
+class Window:
+    def __init__(self,root):
         # title, resolution, background colour
-        self.title("ChatGPT Survey GUI")
-        self.geometry('800x800')
-        self.configure(background="light gray")
-        self.resizable(False,False)
+        self.root = root
 
-        l_title         = Label(self, 
-                                 bg = "light gray", 
+        self.root.title("ChatGPT Survey GUI")
+        self.root.geometry('800x800')
+        self.root.configure(background="light gray")
+        self.root.resizable(False,False)
+
+        l_title         = Label(self.root,
+                                 bg = "light gray",
                                  text= "Click a button to check a graph!", 
                                  font = ("Georgia", 25))
         
         # button initializations
         # add action events for each button later
-        b_age           = Button(self, 
-                                 width = 27, 
-                                 command = lambda: create_pie(AGE[1:],AGE[0]), 
+        b_age           = Button(self.root, 
+                                 width = 25, 
+                                 command = lambda: create_pie(self.root,AGE[1:],AGE[0]), 
                                  text = "How Old?", pady=10)
 
-        b_department    = Button(self, 
-                                 width = 27, 
-                                 command = lambda: create_pie(DEPARTMENT[1:],DEPARTMENT[0]), 
+        b_department    = Button(self.root, 
+                                 width = 25, 
+                                 command = lambda: create_pie(self.root,DEPARTMENT[1:],DEPARTMENT[0]), 
                                  text = "What Department?", pady=10)
 
-        b_heard         = Button(self, 
-                                 width = 27, 
-                                 command = lambda: create_pie(HEARD[1:],HEARD[0]), 
+        b_heard         = Button(self.root, 
+                                 width = 25, 
+                                 command = lambda: create_pie(self.root,HEARD[1:],HEARD[0]), 
                                  text = "Have you Heard of ChatGPT?", pady=10)
         
-        b_usage         = Button(self, 
-                                 width = 27, 
-                                 command = lambda: create_bar(USAGE[1:],USAGE[0]), 
+        b_usage         = Button(self.root, 
+                                 width = 25, 
+                                 command = lambda: create_bar(self.root,USAGE[1:],USAGE[0]), 
                                  text = "How have you Used ChatGPT?", pady=10)
         
-        b_improvement   = Button(self, 
-                                 width = 27, 
-                                 command = lambda: create_pie(IMPROVEMENT[1:],IMPROVEMENT[0]), 
+        b_improvement   = Button(self.root, 
+                                 width = 25, 
+                                 command = lambda: create_pie(self.root,IMPROVEMENT[1:],IMPROVEMENT[0]), 
                                  text = "Any Improvement from ChatGPT?", pady=10)
         
-        b_reliability   = Button(self, 
-                                 width = 27, 
-                                 command = lambda: create_pie(RELIABILITY[1:],RELIABILITY[0]), 
+        b_reliability   = Button(self.root, 
+                                 width = 25, 
+                                 command = lambda: create_pie(self.root,RELIABILITY[1:],RELIABILITY[0]), 
                                  text = "How Reliable is ChatGPT?", pady=10)
         
-        b_learning      = Button(self, 
-                                 width = 27, 
-                                 command = lambda: create_pie(LEARNING[1:],LEARNING[0]), 
+        b_learning      = Button(self.root, 
+                                 width = 25, 
+                                 command = lambda: create_pie(self.root,LEARNING[1:],LEARNING[0]), 
                                  text = "Learnt from ChatGPT?", pady=10)
        
-        b_dishonesty    = Button(self, 
-                                 width = 27, 
-                                 command = lambda: create_pie(DISHONESTY[1:],DISHONESTY[0]), 
+        b_dishonesty    = Button(self.root, 
+                                 width = 25, 
+                                 command = lambda: create_pie(self.root,DISHONESTY[1:],DISHONESTY[0]), 
                                  text = "Academic Dishonesty?", pady=10)
        
 
-        b_assistance    = Button(self, 
-                                 width = 27, 
-                                 command = lambda: create_bar(ASSISTANCE[1:],ASSISTANCE[0]), 
+        b_assistance    = Button(self.root, 
+                                 width = 25, 
+                                 command = lambda: create_bar(self.root,ASSISTANCE[1:],ASSISTANCE[0]), 
                                  text = "What kind of Assistance?", pady=10)
 
         # place where they are on the GUI (x, y)
         l_title.place(x=160, y=0)
-        b_age.place(x=0, y=50)
-        b_department.place(x=200, y=50)
-        b_heard.place(x=400, y=50)
-        b_usage.place(x=600, y=50)
-        b_improvement.place(x=0, y=100)
-        b_learning.place(x=400, y=100)
-        b_reliability.place(x=200, y=100)
-        b_dishonesty.place(x=600, y=100)
+        b_age.place(x=10, y=50)
+        b_department.place(x=210, y=50)
+        b_heard.place(x=410, y=50)
+        b_usage.place(x=610, y=50)
+        b_improvement.place(x=10, y=100)
+        b_learning.place(x=410, y=100)
+        b_reliability.place(x=210, y=100)
+        b_dishonesty.place(x=610, y=100)
         b_assistance.place(x=300, y=150)
 
 if __name__ == '__main__':
-    GUI = Window()
-    GUI.mainloop()
+    root = Tk()
+    GUI = Window(root)
+    GUI.root.mainloop()
